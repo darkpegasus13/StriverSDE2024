7bf35c48ce1a8412cbf376e6b0b2bf683957b42b
jayeshbhushan1309 <jayesh_bhushan@persistent.com>
2024-03-27 18:23:41 +0530

    DP-4

--- StriverSDE/StriverSDE/DynamicProgramming.cs
+++ StriverSDE/StriverSDE/DynamicProgramming.cs
@@ -95,5 +95,97 @@ namespace StriverSDE
         }
 
         #endregion
+
+        #region Longest Common Subsequence
+
+        //Naive Approach S=>O(2^N) and T=>O(2^N)
+        //generate all substring for both and then find the matching
+        //largest substring
+
+        //Better Approach S=>O(N) and T=>O(2^(n+m))
+        //using recursion
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+
+        }
+
+        public int LongestCommonSubsequenceHelper(string text1, string text2, int indx1,int indx2)
+        {
+            if (indx1 < 0 || indx2 < 0)
+                return 0;
+            if (text1[indx1] == text2[indx2])
+                return 1 + LongestCommonSubsequenceHelper(text1, text2, indx1 - 1, indx2 - 1);
+            else
+            {
+                return Math.Max(LongestCommonSubsequenceHelper(text1, text2, indx1, indx2 - 1),
+                    LongestCommonSubsequenceHelper(text1, text2, indx1 - 1, indx2));
+            }
+        }
+
+        //Better Approach S=>O(N)+O(N*M) and T=>O(N*M)
+        //using dp memoization
+
+        public int lcs(int n, int m, string s1, string s2)
+        {
+            //Your code here
+            var memoizationMatrix = new int[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                memoizationMatrix[i] = Enumerable.Repeat(-1, m + 1).ToArray();
+            }
+            return this.LCSDP(s1, s2, n, m, memoizationMatrix);
+        }
+
+        public int LCSDP(string a, string b, int m, int n, int[][] memo)
+        {
+            if (m == 0 || n == 0)
+                return 0;
+            if (memo[m - 1][n - 1] == -1)
+            {
+                int res;
+                if (a[m - 1] == b[n - 1])
+                    res = 1 + LCSDP(a, b, m - 1, n - 1, memo);
+                else
+                    res = Math.Max(this.LCSDP(a, b, m - 1, n, memo), this.LCSDP(a, b, m, n - 1, memo));
+                memo[m - 1][n - 1] = res;
+            }
+            return memo[m - 1][n - 1];
+        }
+
+        //Better Approach S=>O(N)+O(N*M) and T=>O(N*M)
+        //using dp tabulation
+
+        public int LCSDPTab(string text1, string text2)
+        {
+            int n = text1.Length;
+            int m = text2.Length;
+            var memoizationMatrix = new int[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                memoizationMatrix[i] = Enumerable.Repeat(-1, m + 1).ToArray();
+            }
+            for (int i = 0; i <= n; i++)
+                memoizationMatrix[i][0] = 0;
+            for (int j = 0; j <= m; j++)
+                memoizationMatrix[0][j] = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    if (text1[i - 1] == text2[j - 1])
+                        memoizationMatrix[i][j] = 1 + memoizationMatrix[i - 1][j - 1];
+                    else
+                        memoizationMatrix[i][j] = Math.Max(memoizationMatrix[i - 1][j], memoizationMatrix[i][j - 1]);
+                }
+            }
+            return memoizationMatrix[n][m];
+
+        }
+        //Better Approach S=>O(N)+O(N+M) and T=>O(N*M)
+        //using dp tabulation and doing some tabulation
+        //we need only two rows curr and prev
+
+        #endregion
     }
 }
