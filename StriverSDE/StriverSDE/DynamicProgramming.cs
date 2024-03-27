895dbfeafaf206cb8d9e06182b2ab23ce01aaca5
jayeshbhushan1309 <jayesh_bhushan@persistent.com>
2024-03-27 13:00:40 +0530

    dynamic programming start

---
+++ StriverSDE/StriverSDE/DynamicProgramming.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StriverSDE
+{
+    class DynamicProgramming
+    {
+        #region Maximum Product SubArray
+
+        //Naive Solution S=>O(1) and T=>O(N^3)
+        //using 3 nested loops 
+
+        //Better Solution S=>O(1) and T=>O(N^2)
+        //using 2 loops 
+
+        //Optimal Approach 1
+        //by taking product of whole array and handle -ve and zeros
+        //as the greatest sum is obtained by multiply every element in array if we can handle 
+        //-ve  and zeros either the prefix will be the answer of the suffix when we exclude a -ve/0
+
+        public int MaxProduct(int[] nums)
+        {
+            int n = nums.Length;
+            int prefix = 1;
+            int suffix = 1;
+            int max = int.MinValue;
+            for(int i = 0; i < n; i++)
+            {
+                //prefix/suffix has become zero by encountering zero so excluding it
+                if (prefix == 0)
+                    prefix = 1;
+                if (suffix == 0)
+                    suffix = 1;
+                prefix = prefix * nums[i];
+                suffix = suffix * nums[n - i - 1];
+                max = Math.Max(max,Math.Max(prefix,suffix));
+            }
+            return max;
+        }
+        #endregion
+
+        #region Longest Increasing Subsequence
+
+        //Naive Solution S=>O(2^n) and T=>O(2^n)
+        //generate all subsequences and then check the 
+        //LIS
+
+        //Optimal Solution S=>O(N*N)+O(N) and T=>O(N*N) 
+        //Using DP
+        public int LengthOfLIS(int[] nums)
+        {
+            int n = nums.Length;
+            int[][] dp = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = Enumerable.Repeat(-1, n + 1).ToArray();
+            }
+            return GetAns(nums, n, 0, -1, dp);
+        }
+
+        public int GetAns(int[] arr,int n,int ind,int prev_indx,int[][] dp)
+        {
+            if (ind == n)
+                return 0;
+            if (dp[ind][prev_indx + 1] != -1)
+                return dp[ind][prev_indx + 1];
+            int notTake = 0 + GetAns(arr, n, ind + 1, prev_indx, dp);
+            int take = 0;
+            if (prev_indx == -1 || arr[ind] > arr[prev_indx])
+                take = 1 + GetAns(arr,n,ind+1,ind,dp);
+            dp[ind][prev_indx + 1] = Math.Max(notTake,take);
+            return dp[ind][prev_indx + 1];
+        }
+
+        //A good recursive solution written by my intuition but for DP 
+        //maybe not compatible
+        public int LengthOfLISHelper(int[] nums, int indx, List<int> curr)
+        {
+            if (indx >= nums.Length)
+                return curr.Count;
+            if (curr.Count == 0 || curr.Last() < nums[indx])
+            {
+                curr.Add(nums[indx]);
+                var take = LengthOfLISHelper(nums, indx + 1, curr);
+                curr.RemoveAt(curr.Count - 1);
+                var notTake = LengthOfLISHelper(nums, indx + 1, curr);
+                return Math.Max(take, notTake);
+            }
+            else
+                return LengthOfLISHelper(nums, indx + 1, curr);
+        }
+
+        #endregion
+    }
+}
